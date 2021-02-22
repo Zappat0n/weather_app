62ebab66f1035732087b6e1c84a9eed860a8d56a
angel <albarros1973@gmail.com>
2021-02-22 10:10:57 +0100

    Initialize

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Todo List',
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[contenthash].css',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext]',
+    publicPath: '',
+  },
+  watch: true,
+  experiments: {
+    asset: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|svg|jpeg)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024, // 4kb
+          },
+        },
+      },
+    ],
+  },
+};
